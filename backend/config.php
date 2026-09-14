fe8142586c5ba244c61cb12a479ea12a255f4882
sathish <sathisathish1530@gmail.com>
2026-09-14 12:44:44 +0530

    update

--- backend/config.php
+++ backend/config.php
@@ -1,6 +1,6 @@
 <?php
 // Explicitly list PUT and OPTIONS alongside GET and POST to clear browser CORS rules
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
